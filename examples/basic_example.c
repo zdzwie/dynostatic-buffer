707b38f7bf4193bd736f4b75b51ea47fc47f8da9
Jakub Brzezowski <44787968+zdzwie@users.noreply.github.com>
2022-06-16 15:17:21 +0200

    Merge pull request #1 from zdzwie/develop
    root: Adding basic versions of dynostatic-buffer files and one basic …

---
+++ examples/basic_example.c
@@ -0,0 +1,19 @@
+/**
+ * @file basic_example.c
+ * @author Jakub Brzezowski
+ * @copyright Copyright (c) 2022
+ *
+ * @brief Basic example for dynostatic-buffer.
+ * @version 0.1
+ * @date 2022-06-16
+ *
+ */
+
+#include <stdio.h>
+
+#include "dynostatic-buffer.h"
+
+int main(void)
+{
+    printf("TODO: end this example.");
+}
